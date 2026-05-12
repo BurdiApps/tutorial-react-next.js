c1e4308908ae97d91c72dfcace6912f7307f5833
James Burdick <jamesburdick@Jamess-Air.lan>
2026-05-12 00:01:10 -0700

    Finished chapters 8-10 in React Foundations for next.js tutorial

---
+++ nextjs-tutorial/app/like-button.js
@@ -0,0 +1,13 @@
+'use client'; // Need to use client component instead of server component.
+
+import { useState } from 'react';
+
+export default function LikeButton() {
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return <button onClick={handleClick}>Like ({likes})</button>;
+}
